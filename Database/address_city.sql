96c246fd12d67956f76db04511764d44808d8ebd
Cong Nuyen <thanhcong2939gmail.com>
2020-09-20 00:21:06 +0700

    .add database

---
+++ Database/address_city.sql
@@ -0,0 +1,443 @@
+﻿
+
+/* INSERT QUERY NO: 1 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+01, N'Thành phố Hà Nội'
+);
+
+/* INSERT QUERY NO: 2 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+02, N'Tỉnh Hà Giang'
+);
+
+/* INSERT QUERY NO: 3 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+04, N'Tỉnh Cao Bằng'
+);
+
+/* INSERT QUERY NO: 4 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+06, N'Tỉnh Bắc Kạn'
+);
+
+/* INSERT QUERY NO: 5 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+08, N'Tỉnh Tuyên Quang'
+);
+
+/* INSERT QUERY NO: 6 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+10, N'Tỉnh Lào Cai'
+);
+
+/* INSERT QUERY NO: 7 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+11, N'Tỉnh Điện Biên'
+);
+
+/* INSERT QUERY NO: 8 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+12, N'Tỉnh Lai Châu'
+);
+
+/* INSERT QUERY NO: 9 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+14, N'Tỉnh Sơn La'
+);
+
+/* INSERT QUERY NO: 10 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+15, N'Tỉnh Yên Bái'
+);
+
+/* INSERT QUERY NO: 11 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+17, N'Tỉnh Hoà Bình'
+);
+
+/* INSERT QUERY NO: 12 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+19, N'Tỉnh Thái Nguyên'
+);
+
+/* INSERT QUERY NO: 13 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+20, N'Tỉnh Lạng Sơn'
+);
+
+/* INSERT QUERY NO: 14 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+22, N'Tỉnh Quảng Ninh'
+);
+
+/* INSERT QUERY NO: 15 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+24, N'Tỉnh Bắc Giang'
+);
+
+/* INSERT QUERY NO: 16 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+25, N'Tỉnh Phú Thọ'
+);
+
+/* INSERT QUERY NO: 17 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+26, N'Tỉnh Vĩnh Phúc'
+);
+
+/* INSERT QUERY NO: 18 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+27, N'Tỉnh Bắc Ninh'
+);
+
+/* INSERT QUERY NO: 19 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+30, N'Tỉnh Hải Dương'
+);
+
+/* INSERT QUERY NO: 20 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+31, N'Thành phố Hải Phòng'
+);
+
+/* INSERT QUERY NO: 21 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+33, N'Tỉnh Hưng Yên'
+);
+
+/* INSERT QUERY NO: 22 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+34, N'Tỉnh Thái Bình'
+);
+
+/* INSERT QUERY NO: 23 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+35, N'Tỉnh Hà Nam'
+);
+
+/* INSERT QUERY NO: 24 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+36, N'Tỉnh Nam Định'
+);
+
+/* INSERT QUERY NO: 25 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+37, N'Tỉnh Ninh Bình'
+);
+
+/* INSERT QUERY NO: 26 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+38, N'Tỉnh Thanh Hóa'
+);
+
+/* INSERT QUERY NO: 27 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+40, N'Tỉnh Nghệ An'
+);
+
+/* INSERT QUERY NO: 28 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+42, N'Tỉnh Hà Tĩnh'
+);
+
+/* INSERT QUERY NO: 29 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+44, N'Tỉnh Quảng Bình'
+);
+
+/* INSERT QUERY NO: 30 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+45, N'Tỉnh Quảng Trị'
+);
+
+/* INSERT QUERY NO: 31 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+46, N'Tỉnh Thừa Thiên Huế'
+);
+
+/* INSERT QUERY NO: 32 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+48, N'Thành phố Đà Nẵng'
+);
+
+/* INSERT QUERY NO: 33 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+49, N'Tỉnh Quảng Nam'
+);
+
+/* INSERT QUERY NO: 34 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+51, N'Tỉnh Quảng Ngãi'
+);
+
+/* INSERT QUERY NO: 35 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+52, N'Tỉnh Bình Định'
+);
+
+/* INSERT QUERY NO: 36 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+54, N'Tỉnh Phú Yên'
+);
+
+/* INSERT QUERY NO: 37 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+56, N'Tỉnh Khánh Hòa'
+);
+
+/* INSERT QUERY NO: 38 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+58, N'Tỉnh Ninh Thuận'
+);
+
+/* INSERT QUERY NO: 39 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+60, N'Tỉnh Bình Thuận'
+);
+
+/* INSERT QUERY NO: 40 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+62, N'Tỉnh Kon Tum'
+);
+
+/* INSERT QUERY NO: 41 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+64, N'Tỉnh Gia Lai'
+);
+
+/* INSERT QUERY NO: 42 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+66, N'Tỉnh Đắk Lắk'
+);
+
+/* INSERT QUERY NO: 43 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+67, N'Tỉnh Đắk Nông'
+);
+
+/* INSERT QUERY NO: 44 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+68, N'Tỉnh Lâm Đồng'
+);
+
+/* INSERT QUERY NO: 45 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+70, N'Tỉnh Bình Phước'
+);
+
+/* INSERT QUERY NO: 46 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+72, N'Tỉnh Tây Ninh'
+);
+
+/* INSERT QUERY NO: 47 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+74, N'Tỉnh Bình Dương'
+);
+
+/* INSERT QUERY NO: 48 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+75, N'Tỉnh Đồng Nai'
+);
+
+/* INSERT QUERY NO: 49 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+77, N'Tỉnh Bà Rịa - Vũng Tàu'
+);
+
+/* INSERT QUERY NO: 50 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+79, N'Thành phố Hồ Chí Minh'
+);
+
+/* INSERT QUERY NO: 51 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+80, N'Tỉnh Long An'
+);
+
+/* INSERT QUERY NO: 52 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+82, N'Tỉnh Tiền Giang'
+);
+
+/* INSERT QUERY NO: 53 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+83, N'Tỉnh Bến Tre'
+);
+
+/* INSERT QUERY NO: 54 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+84, N'Tỉnh Trà Vinh'
+);
+
+/* INSERT QUERY NO: 55 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+86, N'Tỉnh Vĩnh Long'
+);
+
+/* INSERT QUERY NO: 56 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+87, N'Tỉnh Đồng Tháp'
+);
+
+/* INSERT QUERY NO: 57 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+89, N'Tỉnh An Giang'
+);
+
+/* INSERT QUERY NO: 58 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+91, N'Tỉnh Kiên Giang'
+);
+
+/* INSERT QUERY NO: 59 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+92, N'Thành phố Cần Thơ'
+);
+
+/* INSERT QUERY NO: 60 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+93, N'Tỉnh Hậu Giang'
+);
+
+/* INSERT QUERY NO: 61 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+94, N'Tỉnh Sóc Trăng'
+);
+
+/* INSERT QUERY NO: 62 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+95, N'Tỉnh Bạc Liêu'
+);
+
+/* INSERT QUERY NO: 63 */
+INSERT INTO address_city(city_id, city_name)
+VALUES
+(
+96, N'Tỉnh Cà Mau'
+);
+
